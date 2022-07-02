c57ea52f48885b3652d383d26b98eb7c078d1146
Aasif Nawaz <aasiffiraz178@gmail.com>
2022-07-02 20:36:36 +0530

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const pass = {"password": [
-    {
-        "iv": "460858ed0f756838b704c8baa4810b51",
-        "content": "d2ab8c9a3fd51267075538c0c04af582",
-        "_id": "62bef84b9dd532ebd51a02ae"
-    }
-],}
-
-console.log(pass["password"][0]);
